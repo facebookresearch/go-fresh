b3620f9a9151d54d60b351ce553a5d16bda20691
LinaMezghani <lina.mezghani@gmail.com>
2022-04-15 01:00:34 -0700

    policy learning for walker

--- scripts/grid_search.sh
+++ scripts/grid_search.sh
@@ -2,7 +2,8 @@
 
 python main.py \
     +env=maze_U4rooms \
-    main.suffix="rs\${replay_buffer.reward_scaling}-lr\${sac.optim.lr}-bs\${sac.optim.batch_size}-updates\${sac.optim.num_updates_per_epoch}-alpha\${sac.optim.entropy.alpha}-gamma\${sac.optim.gamma}-rnet_graph_dist-grid_search" \
+    main.suffix="oracle\${main.oracle_reward}-rs\${replay_buffer.reward_scaling}-lr\${sac.optim.lr}-bs\${sac.optim.batch_size}-alpha\${sac.optim.entropy.alpha}-gamma\${sac.optim.gamma}-grid_search" \
+    main.oracle_reward=True,False \
     replay_buffer.reward_scaling=0.01,0.1,0.001 \
     sac.optim.lr=0.0001,0.0003,0.00008 \
     sac.optim.batch_size=1024,2048 \
