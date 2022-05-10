818a3fe2bb6ee641aa98555398af79fdc7e3321c
LinaMezghani <lina.mezghani@gmail.com>
2022-05-10 04:55:01 -0700

    scripts & configs for maze

--- scripts/maze_oracle_asym.sh
+++ scripts/maze_oracle_asym.sh
@@ -3,7 +3,7 @@
 python main.py \
     +exp=maze_rnet_asym \
     main.suffix="rs\${replay_buffer.reward_scaling}-lr\${sac.optim.lr}-bs\${sac.optim.batch_size}-updates\${sac.optim.num_updates_per_epoch}-alpha\${sac.optim.entropy.alpha}-gamma\${sac.optim.gamma}-\${main.reward}-reward-rnet-asym" \
-    main.reward=graph_sig \
+    main.reward=rnet \
     replay_buffer.reward_scaling=0.01,0.1,1.0 \
     sac.optim.lr=0.0003,0.0001,0.0006 \
     sac.optim.batch_size=2048 \
