ca3dbad517515edf41424619ac1ee3b3977e4548
LinaMezghani <lina.mezghani@gmail.com>
2022-05-12 07:32:06 -0700

    lina notebooks and sctips

--- scripts/grid_search.sh
+++ scripts/grid_search.sh
@@ -2,20 +2,19 @@
 
 python main.py \
     +exp=walker_rnet_thresh \
-    main.suffix="\${main.reward}-reward-rs\${replay_buffer.reward_scaling}-lr\${sac.optim.lr}-bs\${sac.optim.batch_size}-alpha\${sac.optim.entropy.alpha}-gamma\${sac.optim.gamma}-action_repeat\${env.action_repeat}-1goal\${eval.goal_idx}" \
-    rnet.model.remove_velocity=True \
-    eval.goal_idx=8,10 \
-    train.goal_strat='one_goal' \
+    main.suffix="\${main.reward}-reward-rs\${replay_buffer.reward_scaling}-lr\${sac.optim.lr}-bs\${sac.optim.batch_size}-alpha\${sac.optim.entropy.alpha}-gamma\${sac.optim.gamma}-unsup-graph_sig-temp\${main.reward_sigm_temp}" \
     main.load_from_dir='/checkpoint/sainbar/offline-gcrl/logs/20220509-173335_walker_memonly_thresh2_neg0_skip0.05' \
+    rnet.model.remove_velocity=True \
     rnet.model.hidden_size=128 \
     rnet.model.comp_n_layers=4 \
     rnet.model.feat_size=128 \
-    main.reward=graph,graph_sig,oracle_sparse \
+    main.reward=graph_sig \
+    main.reward_sigm_temp=1,3,5 \
     env.action_repeat=2 \
-    replay_buffer.reward_scaling=1.0,0.1,0.01 \
-    sac.optim.lr=0.0005,0.0003 \
-    sac.optim.batch_size=2048 \
-    sac.optim.num_updates_per_epoch=1000 \
-    sac.optim.entropy.alpha=0.008,0.01 \
-    sac.optim.gamma=0.85,0.9,0.95,0.99 \
+    eval.interval_epochs=20 \
+    sac.optim.batch_size=2048,512 \
+    replay_buffer.reward_scaling=0.1,0.3,1.0 \
+    sac.optim.lr=0.0005,0.001 \
+    sac.optim.entropy.alpha=0.005,0.01 \
+    sac.optim.gamma=0.9,0.95 \
     --multirun \
