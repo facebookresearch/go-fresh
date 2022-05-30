aad9a69420030ceaac07caa2f5782fb696a0f4df
LinaMezghani <lina.mezghani@gmail.com>
2022-05-30 08:18:24 -0700

    notebook and script

--- scripts/grid_search.sh
+++ scripts/grid_search.sh
@@ -3,13 +3,15 @@
 python main.py \
     +exp=walker_rnet_thresh \
     main.suffix="unsup-grid-vec-\${train.goal_strat}-remove_velocity\${sac.policy.head.remove_velocity}-stack\${env.frame_stack}" \
-    main.load_from_dir='/checkpoint/sainbar/offline-gcrl/logs/20220509-173335_walker_memonly_thresh2_neg0_skip0.05' \
+    main.load_from_dir='/checkpoint/linamezghani/offline-gcrl/trained_rnet/walker_memonly_thresh2_neg0_skip0.05_new-graph' \
     rnet.model.hidden_size=128 \
     rnet.model.comp_n_layers=4 \
     rnet.model.feat_size=128 \
     env.action_repeat=2 \
     main.reward=graph_sig \
-    main.train_until=memory \
+    main.edge_transitions=True \
+    eval.num_episodes=50 \
+    replay_buffer.num_procs=1 \
     #sac.policy.head.remove_velocity=True,False \
     #env.frame_stack=1,2,3,4 \
     #main.run=0,1,2 \
