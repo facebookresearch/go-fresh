a087d47388912131126054b520129496e772ba03
LinaMezghani <lina.mezghani@gmail.com>
2022-05-30 08:18:04 -0700

    fix device of expl_buffer.embs proble

--- rnet/memory.py
+++ rnet/memory.py
@@ -81,6 +81,7 @@ class RNetMemory(GraphMemory):
 
     def build(self, model, expl_buffer):
         assert not model.training
+        expl_buffer.embs = expl_buffer.embs.to(self.device)
         x = torch.from_numpy(expl_buffer.get_obs(0, 0)).to(self.device)
         self.add_first_obs(model, x, expl_buffer.get_state(0, 0))
 
