285e568a756f84f34006608e653eed5e25999249
LinaMezghani <lina.mezghani@gmail.com>
2022-06-09 06:21:49 -0700

    script

--- scripts/maze_graph_sig.sh
+++ scripts/maze_graph_sig.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 python main.py \
-    +exp=maze_rnet_sym \
-    main.suffix="undirected-rs\${replay_buffer.reward_scaling}-gamma\${sac.optim.gamma}" \
-    main.load_from_dir='/checkpoint/linamezghani/offline-gcrl/logs/20220614-090425_maze_U4rooms_rnet_sym_new_s234_r0' \
-    main.reward=graph_sig \
-    main.edge_transitions=True \
-    main.subgoal_transitions=True \
-    main.seed=234,123,345 \
-    replay_buffer.reward_scaling=0.1,0.5 \
-    plot.type=wandb \
-    main.reward_sigm_temp=1 \
-    sac.optim.lr=0.0003,0.0005 \
+    +exp=maze_rnet_asym \
+    main.suffix="rs\${replay_buffer.reward_scaling}-lr\${sac.optim.lr}-alpha\${sac.optim.entropy.alpha}-gamma\${sac.optim.gamma}-\${main.reward}-reward-done-negact-fixQ-rb1M" \
+    main.reward=act_model \
+    replay_buffer.reward_scaling=1.0,0.5,2.0,10.0,50.0,100.0 \
+    sac.optim.lr=0.0003,0.0005,0.0001,0.001 \
     sac.optim.batch_size=2048 \
-    sac.optim.entropy.alpha=0.05 \
+    sac.optim.entropy.alpha=0.05,0.01,0.08,10.005 \
     sac.optim.gamma=0.95,0.99 \
     eval.interval_epochs=10 \
     replay_buffer.num_procs=20 \
