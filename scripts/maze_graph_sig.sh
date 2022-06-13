1557ce088b2a546910f5c9a117cef9b33f8d26f7
LinaMezghani <lina.mezghani@gmail.com>
2022-06-13 09:44:41 -0700

    script for walker AM-HER

--- scripts/maze_graph_sig.sh
+++ scripts/maze_graph_sig.sh
@@ -2,19 +2,20 @@
 
 python main.py \
     +exp=maze_rnet_asym \
-    main.suffix="rs\${replay_buffer.reward_scaling}-lr\${sac.optim.lr}-alpha\${sac.optim.entropy.alpha}-gamma\${sac.optim.gamma}-\${main.reward}-neg-action-\${replay_buffer.neg_action}-neg-goal-\${replay_buffer.neg_goal}-cut100-rb1M" \
+    main.suffix="baseline-HER-final-neg-action-\${replay_buffer.neg_action}-neg-goal-\${replay_buffer.neg_goal}" \
     main.reward=act_model \
-    replay_buffer.neg_action=null,policy,uniform \
-    replay_buffer.neg_goal=zero,critic \
-    replay_buffer.reward_scaling=1,10,50 \
-    sac.optim.lr=0.00005 \
+    replay_buffer.neg_action=null \
+    replay_buffer.neg_goal=zero \
+    replay_buffer.reward_scaling=10 \
+    sac.optim.lr=0.0001 \
     sac.optim.batch_size=2048 \
-    sac.optim.entropy.alpha=0.005 \
+    sac.optim.entropy.alpha=0.001 \
     sac.optim.gamma=0.99 \
     optim.num_epochs=5001 \
+    main.seed=234,123,345 \
     eval.interval_epochs=10 \
     replay_buffer.num_procs=20 \
     plot.type=wandb \
-    hydra.launcher.partition=devlab \
+    hydra.launcher.partition=learnlab \
     hydra.launcher.cpus_per_task=20 \
     --multirun \
