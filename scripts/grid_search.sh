653579851dfc8c6280bb2dea91fbc21c7e247ec0
LinaMezghani <lina.mezghani@gmail.com>
2022-06-13 07:42:02 -0700

    final grid search scipt

--- scripts/grid_search.sh
+++ scripts/grid_search.sh
@@ -2,7 +2,7 @@
 
 python main.py \
     +exp=walker_rnet_thresh \
-    main.suffix="unsup-grid-vec-rs\${replay_buffer.reward_scaling}-lr\${sac.optim.lr}-alpha\${sac.optim.entropy.alpha}-gamma\${sac.optim.gamma}-\${train.goal_strat}-edge-trans\${main.edge_transitions}-subgoal-trans\${main.subgoal_transitions}-with-velo_rb1M" \
+    main.suffix="unsup-final-vec_edge-trans\${main.edge_transitions}-subgoal-trans\${main.subgoal_transitions}-with-velo_rb1M" \
     main.load_from_dir='/checkpoint/linamezghani/offline-gcrl/trained_rnet/walker_memonly_thresh2_neg0_skip0.05_new-graph' \
     rnet.model.hidden_size=128 \
     rnet.model.comp_n_layers=4 \
@@ -11,18 +11,20 @@ python main.py \
     main.reward=graph_sig \
     replay_buffer.capacity=1000000 \
     replay_buffer.num_procs=20 \
-    main.edge_transitions=True \
+    main.edge_transitions=True,False \
     sac.policy.head.remove_velocity=False \
     plot.type=wandb \
     main.seed=234,123,345 \
     train.goal_strat=rb \
-    main.subgoal_transitions=True \
+    main.subgoal_transitions=True,False \
     main.reward_sigm_temp=1 \
     eval.interval_epochs=20 \
     sac.optim.batch_size=2048 \
-    replay_buffer.reward_scaling=0.3,0.5,0.1 \
-    sac.optim.lr=0.0001,0.00005,0.0003 \
-    sac.optim.entropy.alpha=0.001,0.005,0.01 \
-    sac.optim.gamma=0.9,0.95 \
+    replay_buffer.reward_scaling=0.5 \
+    sac.optim.lr=0.0003 \
+    sac.optim.entropy.alpha=0.01 \
+    sac.optim.gamma=0.95 \
     hydra.launcher.cpus_per_task=20 \
+    optim.num_epochs=5001 \
+    hydra.launcher.partition=learnlab \
     --multirun \
