7feee75ba57d7ee41735385d37bb8d6b5edbc844
LinaMezghani <lina.mezghani@gmail.com>
2022-06-14 09:19:27 -0700

    train undirected

--- scripts/maze_graph_sig.sh
+++ scripts/maze_graph_sig.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 python main.py \
-    +exp=maze_rnet_asym \
-    main.suffix="expert-cheap-rs\${replay_buffer.reward_scaling}-lr\${sac.optim.lr}-bs\${sac.optim.batch_size}-updates\${sac.optim.num_updates_per_epoch}-alpha\${sac.optim.entropy.alpha}-gamma\${sac.optim.gamma}-\${main.reward}-reward-temp-\${main.reward_sigm_temp}-subgoal\${main.subgoal_transitions}-asym-fix-seed_cudnn" \
-    main.load_from_dir='/checkpoint/linamezghani/offline-gcrl/logs/20220613-101123_maze_U4rooms_expert-rs0.01-lr0.001-bs1024-updates1000-alpha0.01-gamma0.99-graph_sig-reward-temp-1.0-subgoalTrue-asym-fix-seed_cudnn_s234_r0' \
-    exploration_buffer.data_dir='/checkpoint/linamezghani/offline-gcrl/data/maze_U4rooms/expert'\
+    +exp=maze_rnet_sym \
+    main.suffix='undirected-rs\${replay_buffer.reward_scaling}-gamma\${sac.optim.gamma}' \
+    main.load_from_dir='/checkpoint/linamezghani/offline-gcrl/logs/20220614-090425_maze_U4rooms_rnet_sym_new_s234_r0' \
     main.reward=graph_sig \
     main.edge_transitions=True \
     main.subgoal_transitions=True \
@@ -16,7 +15,7 @@ python main.py \
     sac.optim.batch_size=2048 \
     sac.optim.num_updates_per_epoch=1000 \
     sac.optim.entropy.alpha=0.05 \
-    sac.optim.gamma=0.9,0.95 \
+    sac.optim.gamma=0.9 \
     eval.interval_epochs=10 \
     hydra.launcher.partition=devlab \
     --multirun \
