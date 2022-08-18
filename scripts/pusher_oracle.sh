059e4b1ef2498c51fb82a05bb12c3ea9645c21d8
LinaMezghani <lina.mezghani@gmail.com>
2022-08-18 02:17:06 -0700

    change success thresh for pusher

--- scripts/pusher_oracle.sh
+++ scripts/pusher_oracle.sh
@@ -2,15 +2,16 @@
 
 python main.py \
     +env=pusher \
-    main.suffix="rs\${replay_buffer.reward_scaling}-lr\${sac.optim.lr}-bs\${sac.optim.batch_size}-updates\${sac.optim.num_updates_per_epoch}-alpha\${sac.optim.entropy.alpha}-gamma\${sac.optim.gamma}-\${main.reward}-reward-debug" \
+    main.suffix="lr\${sac.optim.lr}-alpha\${sac.optim.entropy.alpha}-gamma\${sac.optim.gamma}-\${main.reward}-reward" \
     main.reward=oracle_dense \
     replay_buffer.reward_scaling=1.0 \
-    sac.optim.lr=0.0003,0.0001,0.0006 \
+    sac.optim.lr=0.001,0.0006,0.003 \
     sac.optim.batch_size=2048 \
     sac.optim.num_updates_per_epoch=1000 \
-    sac.optim.entropy.alpha=0.08,0.05,0.03 \
-    sac.optim.gamma=0.95,0.9,0.99 \
+    sac.optim.entropy.alpha=0.01,0.03,0.005 \
+    sac.optim.gamma=0.95 \
     replay_buffer.num_procs=20 \
+    hydra.launcher.cpus_per_task=20 \
     eval.interval_epochs=20 \
     plot.type=wandb \
     hydra.launcher.partition=devlab \
