47c84ae1f27aaa2f939f10f5bc006e3179083894
LinaMezghani <lina.mezghani@gmail.com>
2022-09-15 06:49:11 -0700

    conf scripts and notebooks

---
+++ scripts/walker_ours.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python -m offline_gcrl.main \
+    +exp=walker_ours \
+    main.suffix="ours" \
+    plot.type=wandb \
+    main.seed=234,123,345 \
+    hydra.launcher.cpus_per_task=20 \
+    hydra.launcher.partition=devlab \
+    --multirun \
