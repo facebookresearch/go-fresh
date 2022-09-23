51b168aba27c5d5b8423b26176a3a76c7b1b87d9
LinaMezghani <lina.mezghani@gmail.com>
2022-09-23 03:11:37 -0700

    change rnet train for maze

--- scripts/maze_ours.sh
+++ scripts/maze_ours.sh
@@ -3,7 +3,6 @@
 python -m offline_gcrl.main \
     +exp=maze_ours \
     main.suffix=ours_load-rnet \
-    main.load_from_dir=/checkpoint/linamezghani/offline-gcrl/trained_rnet/maze_asym-fixed-new/ \
     plot.type=wandb \
     main.seed=234,123,345 \
     hydra.launcher.cpus_per_task=20 \
