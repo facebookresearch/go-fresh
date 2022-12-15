94ad4236b3a0448fde34a5a202edc514755a6e96
LinaMezghani <lina.mezghani@gmail.com>
2022-12-15 03:08:45 -0800

    adding baseline script for UMaze

---
+++ scripts/maze_AM.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python -m offline_gcrl.main \
+    +exp=maze_AM \
+    replay_buffer.algo=AM,HER,HERu \
+    hydra.launcher.cpus_per_task=20 \
+    eval.interval_epochs=20 \
+    main.seed=234,123,345 \
+    plot.type=wandb \
+    hydra.launcher.partition=devlab \
+    --multirun \
