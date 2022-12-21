235b8db0fb1b81f2a3f126e36c06e8941e6b206d
LinaMezghani <lina.mezghani@gmail.com>
2022-12-21 10:15:13 -0800

    remove scripts docs and outdated configs

--- scripts/walker_ours.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python -m offline_gcrl.main \
-    +exp=walker_ours \
-    # plot.type=wandb \
-    # main.seed=234,123,345 \
-    # hydra.launcher.cpus_per_task=20 \
-    # hydra.launcher.partition=devlab \
-    # --multirun \
